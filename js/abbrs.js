22e87761a69ae843ec836d081210183379a81daf
whurleywhurley <whurleyrhs@gmail.com>
2020-02-20 21:37:20 +0000

    cleaned toolbar

--- js/abbrs.js
+++ js/abbrs.js
@@ -61,10 +61,10 @@ class AbbrTools extends React.PureComponent{
                 <button className="button" onClick={this.inputClick}>Import Abbrs</button>
                 <button className="button" onClick={this.exportToXLS}>Export Abbrs</button>
                 <button className="button" onClick={() => {
-                    if(confirm("Are you sure you want to remove all existing acronyms and replace with a common list?")){
+                    if(confirm("Are you sure you want to remove all existing acronyms and replace with the 55 WG approved list?")){
                         this.importSampleAbbrs();
                     }
-                }}>Load Common Abbrs</button>
+                }}>Load 55 WG Abbrs</button>
             </div>
         );
     }
@@ -112,5 +112,5 @@ class AbbrsViewer extends React.PureComponent {
             </div>
         );
     }
-this.importSampleAbbrs();
 }
+this.importSampleAbbrs();
